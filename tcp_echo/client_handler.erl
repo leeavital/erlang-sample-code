daa12cbd416b82c18d24acf2c66bf3f6ffd80de3
Lee Avital <leeavital@gmail.com>
2014-04-11 16:48:18 -0400

    Remove stuff

--- tcp_echo/client_handler.erl
+++ tcp_echo/client_handler.erl
@@ -2,14 +2,14 @@
 
 -compile( export_all ).
 
-init( Sock ) ->
-  loop( Sock ).
+
+
 
 
 loop( Sock ) ->
   case gen_tcp:recv( Sock, 0 ) of
     {ok, Msg} ->
-      gen_tcp:send( Sock, Msg ),
+      tc_client_pool ! {msg, Msg},
       loop( Sock );
     {error, E} ->
       io:format( "ERROR~p~n", E)
--- tcp_echo/tcp_echo.erl
+++ tcp_echo/tcp_echo.erl
@@ -4,17 +4,19 @@
 -compile( export_all ).
 
 main( Port ) ->
+  Pool = client_pool:init(),
+  register(tc_client_pool, Pool),
   {ok, LSock} = gen_tcp:listen( Port, [binary, {packet, 0}, {active, false} ] ),
-  loop( LSock ).
+  spawn( fun() -> loop( LSock ) end ).
 
 
 
 loop( LSock ) ->
   case gen_tcp:accept( LSock ) of
     {ok, Sock} ->
-      io:format( "accepted a socket" ),
-      spawn( client_handler, init, [Sock] ),
-      loop( LSock );
+      spawn( client_handler, loop, [Sock] ),
+      tc_client_pool ! {add_client, Sock},
+      loop( LSock);
     _ ->
       io:format( "Error?" )
   end.
